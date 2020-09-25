0cbf6be170670578e6b5283f0a3436b7ed64692f
A. L. Seale TenthSpeedWriter <addseale2@gmail.com>
2020-09-24 19:18:50 -0500

    Makes update function an actual function instead of hardcoding the query

--- esisan.py
+++ esisan.py
@@ -9,6 +9,8 @@ DODIXIE = "10000032"
 HEK = "10000042"
 THERA = "11000031"
 
+# Thera is small, so it's useful for development--but could also be a cool market candidate for study.
+# Remember, Thera is a perfectly viable market if you're willing to crash a hole camp. :)
 TARGET_MARKET = THERA
 
 market_regions = (JITA, AMARR, RENS, DODIXIE, HEK)
@@ -17,10 +19,13 @@ API_URL = "https://esi.evetech.net/latest/"
 
 
 def get_orders_for_region(region_id):
+    """Returns a list of one row dict for each outstanding buy or sell order in the specified region."""
     path = API_URL + 'markets/{}/orders/'.format(str(region_id))
     now = dt.strftime(dt.utcnow(), "%Y-%m-%dT%H:%M:%S%Z")
     print("Beginning cache for region " + str(region_id) + " at " + now)
+
     def get_page(page):
+        """Returns a single page of orders using the specified path."""
         params = {
             "datasource": "tranquility",
             "order_type": "all",
@@ -37,6 +42,9 @@ def get_orders_for_region(region_id):
 
     def get_all_pages(page_no=1,
                       pages=[]):
+        """Recursively gathers and compiles order rows from ESI for the specified path.
+
+        Returns a list of row dicts."""
         next_page = get_page(page_no)
 
         if len(pages) == 0:
@@ -53,47 +61,42 @@ def get_orders_for_region(region_id):
             return get_all_pages(page_no + 1,
                                  next_pages)
 
-
-        # if next_page is not []:
-        #     if pages is None:
-        #         next_pages = next_page
-        #     else:
-        #         next_pages = pages + next_page
-        #     print(len(next_pages))
-        #     return get_all_pages(page_no + 1, next_pages)
-        # else:
-        #     print(len(pages))
-        #     return pages
-
+    # Execute request sequence, appending the query timestamp before returning the rows.
     rows = get_all_pages()
     for r in rows:
         r["queried_on"] = now
     return rows
 
 
-live_orders = get_orders_for_region(TARGET_MARKET)
-flat_orders = [(o["order_id"],
-                o["queried_on"],
-                o["location_id"],
-                o["system_id"],
-                o["type_id"],
-                o["duration"],
-                o["issued"],
-                o["range"],
-                o["type"],
-                o["price"],
-                o["volume_total"],
-                o["volume_remain"],
-                o["min_volume"])
-               for o in live_orders]
+def save_orders(region_id=TARGET_MARKET):
+    """Pulls live orders for the target region_id and inserts them into the database."""
+    live_orders = get_orders_for_region(region_id)
 
-with db.make_connection() as conn:
-    c = conn.cursor()
-    query = """REPLACE INTO orders
-               VALUES (?,?,?,?,?,
-                       ?,?,?,?,?,
-                       ?,?,?)"""
-    c.executemany(query, flat_orders)
-    now = dt.strftime(dt.utcnow(), "%Y-%m-%dT%H:%M:%S%Z")
-    print("Update which began at {} has completed at {}".format(live_orders[0]["queried_on"],
-                                                                now))
+    # Pretty sure SQLite can parse dicts as inputs.
+    # There's a more elegant solution than this.
+    flat_orders = [(o["order_id"],
+                    o["queried_on"],
+                    o["location_id"],
+                    o["system_id"],
+                    o["type_id"],
+                    o["duration"],
+                    o["issued"],
+                    o["range"],
+                    o["type"],
+                    o["price"],
+                    o["volume_total"],
+                    o["volume_remain"],
+                    o["min_volume"])
+                   for o in live_orders]
+
+    # TODO: Abstract the DB call to a method in db.py
+    with db.make_connection() as conn:
+        c = conn.cursor()
+        query = """REPLACE INTO orders
+                   VALUES (?,?,?,?,?,
+                           ?,?,?,?,?,
+                           ?,?,?)"""
+        c.executemany(query, flat_orders)
+        now = dt.strftime(dt.utcnow(), "%Y-%m-%dT%H:%M:%S%Z")
+        print("Update which began at {} has completed at {}".format(live_orders[0]["queried_on"],
+                                                                    now))
